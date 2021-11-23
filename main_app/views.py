aef7788de6873056c5178b33849e9478c2f19c8c
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-11-23 11:37:15 -0600

    add basic authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def home(request):
   return HttpResponse('<h1>Home Page</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+class Home(LoginView):
+  template_name = 'home.html'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
