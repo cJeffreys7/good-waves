5b4a14f9edea5f8057db55409362c741d8bf853e
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-11-24 16:00:07 -0600

    style sign up view

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from .models import Review
 
@@ -21,3 +21,19 @@ class CustomAuthForm(AuthenticationForm):
     self.fields['username'].label = False
     self.fields['password'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder':'Password'}) 
     self.fields['password'].label = False
+
+class CustomRegForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ['first_name', 'last_name', 'username','password1', 'password2']
+
+    widgets = {
+      'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+      'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+      'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    }
+
+  def __init__(self, *args, **kwargs):
+    super(CustomRegForm, self).__init__(*args, **kwargs)
+    self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'})
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -10,7 +9,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 
 from main_app.models import RecommendationList, Podcast, Review
-from .forms import ReviewForm
+from .forms import CustomRegForm, ReviewForm
 
 def about(request):
   return render(request, 'about.html')
@@ -21,19 +20,14 @@ class Home(LoginView):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
+    form = CustomRegForm(request.POST)
     if form.is_valid():
-      # This will add the user to the database
       user = form.save()
-      # This is how we log a user in
       login(request, user)
       return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
+  form = CustomRegForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'signup.html', context)
 
