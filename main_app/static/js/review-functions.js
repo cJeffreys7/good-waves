78a745a11c044d382a0e0f0ddaadbe6ae689843e
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-11-27 21:35:41 -0600

    style Podcast Details view

---
+++ main_app/static/js/review-functions.js
@@ -0,0 +1,22 @@
+const ratingStars = document.querySelectorAll(".rating-star")
+const starRating = document.querySelector("#id_rating")
+
+ratingStars.forEach(star => {
+  star.addEventListener("click", setStarFill)
+})
+
+function init(){
+  starRating.value = 5
+}
+
+function setStarFill(evt){
+  const rating = parseInt(evt.target.id.split('-')[1])
+  starRating.value = rating
+  ratingStars.forEach((star, idx) => {
+    if (rating > idx) {
+      star.style = "color: var(--star-fill);"
+    } else {
+      star.style = "color: lightgray;"
+    }
+  })
+}
