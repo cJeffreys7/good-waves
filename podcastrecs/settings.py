878aeed0798d090a290b640f995f47abb6880655
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-11-29 11:29:26 -0600

    configure deployment

--- podcastrecs/settings.py
+++ podcastrecs/settings.py
@@ -26,11 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'hardwire-fe2gc6q(w_zbb1-)^m4y-spiderman-jAms-+_jg*j86qt-jxyl56qhfni6qw-#s*#'
+
+# print(env('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG') == 'TRUE'
+DEBUG = True
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
