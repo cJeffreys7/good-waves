df9e05ddc8a8637d86cd262333c6cb4fb828ba19
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2021-11-29 19:57:14 -0600

    add link preview from Podcast link

---
+++ main_app/static/js/podcast-functions.js
@@ -0,0 +1,49 @@
+const titleInput = document.querySelector("#id_title")
+const descriptionInput = document.querySelector("#id_description")
+const linkInput = document.querySelector("#id_link")
+const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
+const websiteRegEx = new RegExp(expression)
+
+let linkPreviewDelay
+let isDelayed = false
+
+linkInput.addEventListener("input", generateLinkPreview)
+
+function generateLinkPreview(){
+  const inputText = linkInput.value
+  // Determine if input is a valid web address
+  if (inputText.match(websiteRegEx)) {
+    // Interrupt api call delay if input is registered before delay finishes
+    if (isDelayed) {
+      interruptLinkPreviewDelay()
+    }
+    isDelayed = true
+    linkPreviewDelay = setTimeout(function(){
+      fetch(`http://api.linkpreview.net/?key=7c5dc8db90501ddb89b060074bcd94f3&q=${linkInput.value}`
+      )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        if (data.title) {
+          titleInput.value = data.title
+        }
+        if (!data.description.includes('Invalid') && data.description) {
+          descriptionInput.value = data.description
+        }
+      })
+      .catch(err => {
+        console.log('Error:', err)
+      })
+    }, 1500)
+  } else {
+    // Stop api call if invalid web address is entered after a valid web address was previously entered
+    if (isDelayed) {
+      isDelayed = false
+      interruptLinkPreviewDelay()
+    }
+  }
+}
+
+function interruptLinkPreviewDelay(){
+  clearTimeout(linkPreviewDelay)
+}
